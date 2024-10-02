073d41bc1bedd8afff1c4cce0242057e2a25b302
Wahyu NT <wahyuntx28@gmail.com>
2024-10-03 04:08:31 +0700

    Refactor landing page: Add ship-2 element

--- resources/views/livewire/landing.blade.php
+++ resources/views/livewire/landing.blade.php
@@ -118,6 +118,7 @@
             <div style="position: absolute; bottom: 0; left: 0;" class="wave wave-1"></div>
             <div style="position: absolute; bottom: 0; left: 0;" class="ship ship-1"></div>
             <div style="position: absolute; bottom: 0; left: 0;" class="wave wave-2"></div>
+            <div style="position: absolute; bottom: 0; left: 0;" class=" ship-2"></div>
             <div style="position: absolute; bottom: 0; left: 0;" class="wave wave-3"></div>
         </div>
     </section>
