da47185b0651dc53fb25f1db6720a465afb6316c
Wahyu NT <wahyuntx28@gmail.com>
2024-10-02 16:25:41 +0700

    Refactor CSS styles and layout in master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -316,6 +316,9 @@
                     </div>
                 </div>
             </div>
+            <div class="maps mt-5">
+                <iframe class="borad-15" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21041.83286957594!2d110.36639850462139!3d-7.782987181025065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a591a4d553bd5%3A0xc0f964003add568b!2sTugu%20Jogja!5e1!3m2!1sid!2sid!4v1727860992036!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
         </div>
     </section>
 
