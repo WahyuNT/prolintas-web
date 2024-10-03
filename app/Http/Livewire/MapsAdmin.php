62dc08c32d3a829eb4db3c4fb67185607f9fd0f0
Wahyu NT <wahyuntx28@gmail.com>
2024-10-03 14:57:06 +0700

    Refactor MapsAdmin component: Add edit functionality and update save method

--- app/Http/Livewire/MapsAdmin.php
+++ app/Http/Livewire/MapsAdmin.php
@@ -2,12 +2,63 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Landing;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
 
 class MapsAdmin extends Component
 {
+
+    use LivewireAlert;
+    public $maps;
+    public $edit = 'disabled';
+    public $disabled = 'disabled';
+
+
     public function render()
     {
-        return view('livewire.maps-admin');
+        $data = Landing::where('type', 'maps')->first();
+        $maps = $data->maps_link;
+        $this->maps = $maps;
+
+
+        return view('livewire.maps-admin', compact('data', 'maps'));
+    }
+
+
+    public function edit()
+    {
+
+        $this->edit = '';
+    }
+    public function stopEdit()
+    {
+        $this->edit = 'disabled';
+    }
+    public function simpan()
+    {
+        $this->edit = 'disabled';
+        $data = Landing::where('type', 'maps')->first();
+     
+
+        $iframeTag = $this->maps;
+
+      
+        preg_match('/https[^"]*/', $iframeTag, $matches);
+
+        // Cek apakah match ditemukan
+        if (isset($matches[0])) {
+            $data->maps_link = $matches[0]; 
+        } else {
+            $data->maps_link = null;
+        }
+
+     
+
+        if ($data->save()) {
+            $this->alert('success', 'Data berhasil dihapus');
+        } else {
+            $this->alert('error', 'Data gagal dihapus');
+        }
     }
 }
--- resources/views/livewire/maps-admin.blade.php
+++ resources/views/livewire/maps-admin.blade.php
@@ -2,6 +2,32 @@
     <div class="card border-0 shadow-sm borad-15">
         <div class="card-body">
             <h5 class="fw-bold text-center">Maps</h5>
+            <div class="d-flex justify-content-between flex-wrap">
+                <div class="col-12 d-flex justify-content-between align-items-center">
+                    <div class="col-11">
+
+                        <div class="mb-3">
+                            <label for="maps" class="form-label">Maps</label>
+                            <textarea {{ $edit }} name="maps" class="form-control" wire:model.defer="maps" id="maps"></textarea>
+                        </div>
+                    </div>
+                    <div class="col-1 ps-2 text-end">
+                        @if ($edit == 'disabled')
+                            <button wire:click="edit" class="btn btn-secondary">Edit</button>
+                        @else
+                            <button wire:click="simpan" class="btn btn-primary">Simpan</button>
+                        @endif
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="mb-3">
+                        <iframe class="borad-15" src="{{ $maps }}" width="100%" height="450"
+                            style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
